6f912f2e27b3cad5e8c4939d479b2a6137a3fa87
Artsiom Antropau <artsiom.antropau@gcore.com>
2023-10-19 18:35:15 +0200

    GCLOUD2-11719: Add possibility to attach FIP

--- gcore/resource_gcore_loadbalancerv2.go
+++ gcore/resource_gcore_loadbalancerv2.go
@@ -103,6 +103,11 @@ func resourceLoadBalancerV2() *schema.Resource {
 				Description: "Load balancer IP address",
 				Computed:    true,
 			},
+			"vip_port_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Description: "Load balancer Port ID",
+				Computed:    true,
+			},
 			"last_updated": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
@@ -220,6 +225,9 @@ func resourceLoadBalancerV2Read(ctx context.Context, d *schema.ResourceData, m i
 
 	if lb.VipAddress != nil {
 		d.Set("vip_address", lb.VipAddress.String())
+	}
+	if lb.VipPortID != nil {
+		d.Set("vip_port_id", lb.VipPortID.String())
 	}
 
 	fields := []string{"vip_network_id", "vip_subnet_id"}
