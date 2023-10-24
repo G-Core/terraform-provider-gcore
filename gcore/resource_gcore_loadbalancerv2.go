3c60acdb12ab834d617867b7b3b1c99c666c266e
Alexandr Gorshunov <alexandr.gorshunov@gcore.com>
2023-10-24 08:02:54 +0200

    update go.sum

--- gcore/resource_gcore_loadbalancerv2.go
+++ gcore/resource_gcore_loadbalancerv2.go
@@ -222,7 +222,7 @@ func resourceLoadBalancerV2Read(ctx context.Context, d *schema.ResourceData, m i
 	d.Set("region_id", lb.RegionID)
 	d.Set("name", lb.Name)
 	d.Set("flavor", lb.Flavor.FlavorName)
-    d.Set("vip_port_id", lb.VipPortID)
+	d.Set("vip_port_id", lb.VipPortID)
 
 	if lb.VipAddress != nil {
 		d.Set("vip_address", lb.VipAddress.String())
