4c6ddad23452466d926703a2c12cddb063f8ef03
Mateusz Lapinski <mateusz.lapinski@gcore.com>
2024-02-28 14:19:58 +0100

    Add ability to resize LB

--- gcore/resource_gcore_loadbalancerv2.go
+++ gcore/resource_gcore_loadbalancerv2.go
@@ -92,7 +92,7 @@ func resourceLoadBalancerV2() *schema.Resource {
 			},
 			"flavor": &schema.Schema{
 				Type:        schema.TypeString,
-				Description: "Desired flavor to be used for load balancer. Changing this value will re-create load balancer. By default, `lb1-1-2` will be used. ",
+				Description: "Desired flavor to be used for load balancer. By default, `lb1-1-2` will be used. ",
 				Optional:    true,
 			},
 			"vip_network_id": &schema.Schema{
