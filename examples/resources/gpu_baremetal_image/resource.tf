6140122d03d656d24f6cf24f8bfd931a416685b7
Algis Dumbris <algis.dumbris@gcore.com>
2025-02-18 11:45:42 +0200

    Add example resources for GPU baremetal and virtual images
    - Created example Terraform configuration for `gcore_gpu_baremetal_image`
    - Created example Terraform configuration for `gcore_gpu_virtual_image`
    - Demonstrated usage of GPU image resources with Ubuntu 23.10 cloud image

---
+++ examples/resources/gpu_baremetal_image/resource.tf
@@ -0,0 +1,21 @@
+provider "gcore" {
+  permanent_api_token = "251$d3361.............1b35f26d8"
+}
+
+data "gcore_project" "project" {
+  name = "Default"
+}
+
+data "gcore_region" "region" {
+  name = "Luxembourg-2"
+}
+
+resource "gcore_gpu_baremetal_image" "example" {
+  project_id   = data.gcore_project.project.id
+  region_id    = data.gcore_region.region.id
+  name         = "my-ubuntu-23.10-x64"
+  url          = "https://cloud-images.ubuntu.com/releases/23.10/release/ubuntu-23.10-server-amd64.qcow2"
+  architecture = "x86_64"
+  os_type      = "linux"
+  ssh_key      = "allow"
+} 
