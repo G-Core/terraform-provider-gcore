947eda7615179ac7f42a78175fdeeb6fcaeaf667
Izabella Merkulova <izabella.merkulova@gcore.com>
2025-02-25 16:35:27 +0000

    GCLOUD2-17975: add description and examples for gpu cloud

---
+++ examples/resources/gcore_ai_cluster/main.tf
@@ -0,0 +1,34 @@
+provider gcore {
+  permanent_api_token = "251$d3361.............1b35f26d8"
+}
+
+data "gcore_project" "project" {
+  name = "Default"
+}
+
+data "gcore_region" "region" {
+  name = "Luxembourg-2"
+}
+
+resource "gcore_network" "network" {
+  name       = "my-network"
+  type       = "vlan"
+  project_id = data.gcore_project.project.id
+  region_id  = data.gcore_region.region.id
+}
+
+resource "gcore_subnet" "subnet" {
+  name            = "my-subnet"
+  cidr            = "192.168.10.0/24"
+  network_id      = gcore_network.network.id
+
+  project_id = data.gcore_project.project.id
+  region_id  = data.gcore_region.region.id
+}
+
+resource "gcore_keypair" "keypair" {
+  project_id  = data.gcore_project.project.id
+  sshkey_name = "my-keypair"
+  public_key  = "ssh-ed25519 ...your public key... gcore@gcore.com"
+}
+
