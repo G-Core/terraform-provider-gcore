6a0f63682ac5d75f2973713d880a6d1bee49e02e
Paweł Zarębski <ppjzarebski@gmail.com>
2025-10-06 00:14:29 +0200

    CDM-1789 Extend QueryStringForwarding option

--- examples/resources/gcore_cdn_rule/example_2.tf
+++ examples/resources/gcore_cdn_rule/example_2.tf
@@ -145,8 +145,10 @@ resource "gcore_cdn_rule" "cdn_example_com_rule_1" {
       ]
     }
     query_string_forwarding {
-      forward_from_file_types = ["m3u8", "m3u"],
+      forward_from_file_types = ["m3u8", "m3u"]
       forward_to_file_types = ["m3u8", "m3u", "ts", "m4s"]
+      forward_only_keys = ["auth_token", "session_id"]
+      forward_except_keys = ["debug_info"]
     }
     redirect_https_to_http {
       enabled = false
