d9a25b3fa2bdfff1543e16816cae8a47b141b697
Pedro Oliveira <pedro.oliveira@gcore.com>
2025-10-14 15:29:39 +0100

    Update vast file share resource example

--- examples/resources/gcore_file_share/vast.tf
+++ examples/resources/gcore_file_share/vast.tf
@@ -10,7 +10,6 @@ data "gcore_region" "region" {
   name = "Sines-2"
 }
 
-
 resource "gcore_file_share" "file_share_vast" {
   name       = "tf-file-share-vast"
   size       = 10
@@ -18,4 +17,9 @@ resource "gcore_file_share" "file_share_vast" {
   region_id  = data.gcore_region.region.id
   type_name  = "vast"
   protocol   = "NFS"
+  share_settings {
+    allowed_characters = "LCD"
+    path_length = "LCD"
+    root_squash = true
+  }
 }
