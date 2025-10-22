59fc145f6f10cbebcc5027762a757010e948f940
Artsiom Antropau <artsiom.antropau@gcore.com>
2025-10-22 02:15:15 +0200

    GCLOUD2-21656: TF add resource for managing allowed address pairs for any port

--- gcore/provider.go
+++ gcore/provider.go
@@ -208,6 +208,7 @@ func Provider() *schema.Provider {
 			"gcore_waap_firewall_rule":            resourceWaapFirewallRule(),
 			"gcore_file_share":                    resourceFileShare(),
 			"gcore_postgres_cluster":              resourcePostgresCluster(),
+			"gcore_port_allowed_address_pairs":    resourceGcorePortAllowedAddressPairs(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"gcore_ai_cluster":                 dataSourceAICluster(),
